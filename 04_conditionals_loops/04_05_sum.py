b5f69c1e58fdaabb66d218a1c18e596828734a4e
Martin Breuss <martin-martin@users.noreply.github.com>
2019-07-24 16:28:40 +0930

    Update 04_05_sum.py

--- 04_conditionals_loops/04_05_sum.py
+++ 04_conditionals_loops/04_05_sum.py
@@ -1,7 +1,8 @@
 '''
-Take two numbers from the user, an upper and lower bound. Using a loop, calculate the sum
-	of numbers from the lower bound to the upper bound.
+Take two numbers from the user, an upper and lower bound.
+Using a loop, calculate the sum of all numbers from the lower bound to the upper bound.
 
-	For example, if a user enters 1 and 100, the output should be:
-		The sum is: 5050
+For example, if a user enters 1 and 100, the output should be:
+
+The sum is: 5050
 '''
