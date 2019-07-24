331372640a5942a9e65cc3b5696d494fb13cdf12
Martin Breuss <martin-martin@users.noreply.github.com>
2019-07-24 16:23:06 +0930

    Update formulation

--- 01_python_fundamentals/01_02_seconds_years.py
+++ 01_python_fundamentals/01_02_seconds_years.py
@@ -1,5 +1,6 @@
 '''
 
-From the previous example, move your calculation of how many seconds in a year to a python executable script.
+Write the code to calculate how many seconds are in a year in this file,
+so you can execute it as a script and receive the output to your console.
 
 '''
