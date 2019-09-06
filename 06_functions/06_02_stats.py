9407578ac957c91556ff5fad945f4b40e7d8ef95
Martin Breuss <martin-martin@users.noreply.github.com>
2019-09-06 16:02:52 +0930

    Clarify wording

--- 06_functions/06_02_stats.py
+++ 06_functions/06_02_stats.py
@@ -1,4 +1,13 @@
 '''
-Write a script that takes in a list and finds the max, min, average and sum.
+Write a function stats() that takes in a list of numbers and finds the max, min, average and sum.
+Print these values to the console when calling the function.
 
 '''
+
+example_list = [1, 2, 3, 4, 5, 6, 7]
+
+def stats():
+  # define the function here
+  pass
+
+# call the function below here
