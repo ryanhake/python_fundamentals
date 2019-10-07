6c814ef586e5323834afee8496745f8ab22eda0b
Martin Breuss <martin-martin@users.noreply.github.com>
2019-10-07 13:42:32 +1030

    Add note

--- 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
+++ 03_more_datatypes/3_tuples/03_16_pairing_tuples.py
@@ -4,8 +4,10 @@ Write a script that takes in a list of numbers and:
     - stores the numbers in tuples of two in a list
     - prints each tuple
 
-Notes:
 If the user enters an odd numbered list, add the last item
 to a tuple with the number 0.
+
+Note: This lab might be challenging! Make sure to discuss it with your mentor
+or chat about it on our forum.
 
 '''
