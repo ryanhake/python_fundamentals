d05c1be75d5eb199bb96af4d2a280a0f7bbf064e
Gilad Gressel <giladgressel@users.noreply.github.com>
2020-08-11 14:54:25 +0530

    fixed confusing instructions (#3)
    changes made:
    
    OLD: - `Each class should have an __init__ method that sets attributes
        to a default value if values are not passed.  `
    This is confusing because you don't _need_ to have default values, you could have required values as well.
    
    OLD: `- Each object should have at least three attributes.` -- this line removed
    OLD: `- Each class should have at least two class attributes.`  -- this line removed. I don't believe that we are asking for class attributes? did we mean instance attributes? 
    
    OLD: `Include a __str__ method in each class` combined with "create a print method" because that line of instruction seemed to be the same as the next line

--- 07_classes_objects_methods/07_03_freeform.py
+++ 07_classes_objects_methods/07_03_freeform.py
@@ -1,13 +1,9 @@
 '''
 - Write a script with three classes that model everyday objects.
-- Each class should have an __init__ method that sets attributes
-    to a default value if values are not passed.
+- Each class should have an __init__ method that sets at least three attributes
 - Create at least two objects of each class using the __init__ method.
-- Each object should have at least three attributes.
-- Each class should have at least two class attributes.
-- Create a print method in each class that prints out the attributes
+- Include a __str__ method in each class that prints out the attributes
     in a nicely formatted string.
-- Include a __str__ method in each class.
 - Overload the __add__ method in one of the classes so that it's possible
     to add attributes of two instances of that class using the + operator.
 - Once the objects are created, change some of the attribute values.
@@ -15,6 +11,5 @@
 Be creative. Have some fun. :)
 Using objects you can model anything you want.
 Cars, animals, poker games, sports teams, trees, beers, people etc...
-
 
 '''
