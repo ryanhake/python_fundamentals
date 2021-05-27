4a8312f716357a1c5912012aaad25e7f2290a5d8
Ryan Hake <ryanthake1@gmail.com>
2021-05-27 15:31:17 -0400

    var *args 5/27

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def football_teams(*args):
+    for team in args:
+        print(f"$ {team}")
+
+
+football_teams("Giants", "Bucs", "Jets")
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+
+def my_function(**kwarg):
+    for item, calories in kwarg.items():
+        print(f"{calories} calories in {item}.")
+
+
+my_function(oreos=300, banana=20, chips=120)
