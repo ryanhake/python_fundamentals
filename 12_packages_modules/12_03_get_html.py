a82090b3a652730ab0f8040f4a01fc1ef94fb565
Ryan Hake <ryanthake1@gmail.com>
2021-06-07 12:24:51 -0400

    packages labs 6/7

--- 12_packages_modules/12_03_get_html.py
+++ 12_packages_modules/12_03_get_html.py
@@ -10,8 +10,19 @@ TIP:
 
 '''
 import urllib.request
-from bs4 import BeautifulSoup
-page = urllib.request.urlopen("https://codingnomads.co/").read()
-html = BeautifulSoup(page, "html.parser")
+import urllib.parse
+import re
 
-print(html.get_text())
+url = 'https://pythonprogramming.net/'
+values = {'s': 'basics',
+          'submit': 'search'}
+data = urllib.parse.urlencode(values)
+data = data.encode('utf-8')
+req = urllib.request.Request(url, data)
+resp = urllib.request.urlopen(req)
+respData = resp.read()
+#print(respData)
+
+paragraphs = re.findall(r'<p>(.*?)<p>', str(respData))
+for eachP in paragraphs:
+    print(eachP)
