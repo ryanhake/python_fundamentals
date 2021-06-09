ffce6690b58c1130b69ab1b7e278284c02c63ec7
Ryan Hake <ryanthake1@gmail.com>
2021-06-09 13:27:04 -0400

    decorator labs 6/9

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -1,3 +1,16 @@
 """
 Write a decorator function that wraps text passed to it in a html <p> tag.
 """
+
+
+def para(func):
+    def wrapper():
+        return "<p> " + func() + " <p>"
+    return wrapper
+
+@para
+def formatted_text():
+    return "Hello World"
+
+
+print(formatted_text())
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,16 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+
+def para(func):
+    def wrapper(*args):
+        return func(*args)
+    return wrapper
+
+@para
+def formatted_text(tags, msg):
+    return (f"{tags} {msg} {tags}")
+
+
+print(formatted_text("<i>", "hello world"))
